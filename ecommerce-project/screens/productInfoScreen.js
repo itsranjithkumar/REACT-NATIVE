d659d2457cc67c06b4d20093e50622491d5b0a94
iamgowri <gowrishankari27800@gmail.com>
2024-05-01 11:34:44 +0530

    bug

--- ecommerce-project/screens/productInfoScreen.js
+++ ecommerce-project/screens/productInfoScreen.js
@@ -1,18 +1,32 @@
 import { StyleSheet, View, Text, ScrollView, Pressable, TextInput, ImageBackground, Dimensions, } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import { AntDesign, Feather } from "@expo/vector-icons";
 import { useRoute } from '@react-navigation/native';
 import { useNavigation } from '@react-navigation/native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart } from '../redux/CartReducer';
+
 
 
 const productInfoScreen = () => {
     const route = useRoute();
     const { width } = Dimensions.get("window");
     const Navigation = useNavigation();
+    const [addedToCart, setAddedToCart] = useState(false)
     const height = (width * 100) / 100;
     const dispatch = useDispatch();
+    const addItemsToCart = (item) => {
+        setAddedToCart(true);
+        dispatch(addToCart(item));
+        setTimeout(() => {
+            setAddedToCart(false);
+        }, 60000)
+
+    }
+    const cart = useSelector((state) => state.cart.cart);
+    console.log(cart)
     return (
         <ScrollView style={{ marginTop: 45, flex: 1, backgroundColor: "white" }} showsVerticalScrollIndicator={false}>
             <View
@@ -26,7 +40,7 @@ const productInfoScreen = () => {
                 <Pressable
                     style={{
                         flexDirection: "row",
-                        aliignItems: "center",
+                        alignItems: "center",
                         marginHorizontal: 7,
                         gap: 10,
                         backgroundColor: "white",
@@ -65,62 +79,85 @@ const productInfoScreen = () => {
                                     : "center", flexDirection: "row"
                             }}>
 
-                            <MaterialCommunityIcons name="share-variant" size={24} color="black" />
+                                <MaterialCommunityIcons name="share-variant" size={24} color="black" />
                             </View>
                         </View>
-                            
+
 
                         <View style={{
-                                width: 40, height: 40, borderRadius: 20, backgroundColor: "#E0E0E0", justifyContent: "center", alignItems
-                                    : "center", flexDirection: "row",marginTop:"auto",marginLeft:20,marginBottom:20
-                            }}>
-                        <AntDesign name="hearto" size={24} color="black" />
+                            width: 40, height: 40, borderRadius: 20, backgroundColor: "#E0E0E0", justifyContent: "center", alignItems
+                                : "center", flexDirection: "row", marginTop: "auto", marginLeft: 20, marginBottom: 20
+                        }}>
+                            <AntDesign name="hearto" size={24} color="black" />
                         </View>
                     </ImageBackground>
                 ))}
 
             </ScrollView >
-            <View style={{padding:10}}>
-                <Text style={{fontSize:15,fontWeight:"500"}}>{route?.params?.title}</Text>
+            <View style={{ padding: 10 }}>
+                <Text style={{ fontSize: 15, fontWeight: "500" }}>{route?.params?.title}</Text>
 
-                <Text style={{fontSize:18,fontWeight:"600",marginTop:6}}>₹{route?.params?.price}</Text>
+                <Text style={{ fontSize: 18, fontWeight: "600", marginTop: 6 }}>₹{route?.params?.price}</Text>
 
             </View>
 
-            <Text style={{height:1,borderColor:"#D0D0D0",borderWidth: 1 }}/>
+            <Text style={{ height: 1, borderColor: "#D0D0D0", borderWidth: 1 }} />
 
 
-            <View style={{flexDirection:"row",alignItems:"center",padding:10}}> 
+            <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
                 <Text>Color:</Text>
-                <Text style={{fontSize:15,fontWeight:"bold"}}>{route?.params?.color}</Text>
+                <Text style={{ fontSize: 15, fontWeight: "bold" }}>{route?.params?.color}</Text>
             </View>
 
-            <View style={{flexDirection:"row",alignItems:"center",padding:10}}>
+            <View style={{ flexDirection: "row", alignItems: "center", padding: 10 }}>
                 <Text>Size:</Text>
-                <Text style={{fontSize:15,fontWeight:"bold"}}>{route?.params?.size}</Text>
+                <Text style={{ fontSize: 15, fontWeight: "bold" }}>{route?.params?.size}</Text>
             </View>
 
-            <Text style={{height:1,borderColor:"#D0D0D0",borderWidth: 1 }}/>
+            <Text style={{ height: 1, borderColor: "#D0D0D0", borderWidth: 1 }} />
 
-            <View style={{padding:10}}>
-                     <Text style={{fontSize:15,fontWeight:"bold",marginVertical:5}}>Total yarn add @reduxjs/toolkit: ₹{route.params.price}</Text>
-                     <Text style={{color:"#00CED1"}}>Free delivery Tomorrow by 3 PM.Order within 10hrs 30 mins</Text>
+            <View style={{ padding: 10 }}>
+                <Text style={{ fontSize: 15, fontWeight: "bold", marginVertical: 5 }}>Total yarn add @reduxjs/toolkit: ₹{route.params.price}</Text>
+                <Text style={{ color: "#00CED1" }}>Free delivery Tomorrow by 3 PM.Order within 10hrs 30 mins</Text>
 
-                     <View style={{flexDirection:"row",marginVertical:5,alignItems:"center",gap:5}}>
-                     <Ionicons name="location" size={24} color="black" />
-                     <Text style={{fontSize:15,fontWeight:"500"}}>Deliver To Ranjith - Bangalore 560019</Text>
-                     </View>
+                <View style={{ flexDirection: "row", marginVertical: 5, alignItems: "center", gap: 5 }}>
+                    <Ionicons name="location" size={24} color="black" />
+                    <Text style={{ fontSize: 15, fontWeight: "500" }}>Deliver To Ranjith - Bangalore 560019</Text>
+                </View>
             </View>
 
-           <Text style={{color:"green",marginHorizontal:10,fontWeight:"500"}}>IN Stock</Text>
+            <Text style={{ color: "green", marginHorizontal: 10, fontWeight: "500" }}>IN Stock</Text>
 
-           <Pressable style={{backgroundColor:"#FFC72C",padding:18,borderRadius:20,justifyConent:"center",alignItems:"center",marginHorizontal:10,marginVertical:14}}> 
-               <Text>Add to Cart</Text>
-           </Pressable>
+            <Pressable
+                onPress={() => addItemsToCart(route?.params?.items)}
+                style={{
+                    backgroundColor: "#FFC72C",
+                    padding: 18,
+                    borderRadius: 20,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginHorizontal: 10,
+                    marginVertical: 14
+                }}>
 
-           <Pressable style={{backgroundColor:"#FFAC1C",padding:18,borderRadius:20,justifyConent:"center",alignItems:"center",marginHorizontal:10,marginVertical:14}}> 
-               <Text>Buy Now</Text>
-           </Pressable>
+                {addedToCart ? (
+                    <View>
+                        <Text>Added to cart</Text>
+                    </View>
+
+                ) : (
+                    <Text>Add to Cart</Text>
+
+                )}
+
+
+
+                {/* <Text>Add to Cart</Text> */}
+            </Pressable>
+
+            <Pressable style={{ backgroundColor: "#FFAC1C", padding: 18, borderRadius: 20, justifyContent: "center", alignItems: "center", marginHorizontal: 10, marginVertical: 14 }}>
+                <Text>Buy Now</Text>
+            </Pressable>
         </ScrollView>
     );
 };
