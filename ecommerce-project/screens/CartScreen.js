de934dfe7e2c3a88c9f7c00de1f5316655dc4baa
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-03 18:09:49 +0530

    4:56:05

--- ecommerce-project/screens/CartScreen.js
+++ ecommerce-project/screens/CartScreen.js
@@ -8,95 +8,176 @@ import {
   Image,
 } from "react-native";
 import React from "react";
-import { Feather, AntDesign } from "@expo/vector-icons";
-import { useSelector } from "react-redux";
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from '@expo/vector-icons';
+import { useDispatch, useSelector } from "react-redux";
+import { incemetQuantity } from "../redux/CartReducer";
 
 const CartScreen = () => {
-  const cart = useSelector((state) => state.cart.cart);
-  console.log(cart);
+    const cart = useSelector((state) => state.cart.cart);
+    console.log(cart);
   const total = cart
     ?.map((item) => item.price * item.quantity)
     .reduce((curr, prev) => curr + prev, 0);
 
-  console.log(total);
-  return (
-    <ScrollView style={{ marginTop: 55, flex: 1, backgroundColor: "white" }}>
-      <View
-        style={{
-          backgroundColor: "#00CED1",
-          flexDirection: "row",
-          alignItems: "center",
+  const dispatch = useDispatch();
+  const increaseQuantity = (item) => (
+    dispatch(incemetQuantity(item))
+  )
+    return (
+        <ScrollView style={{ marginTop: 55, flex: 1, backgroundColor: "white" }}>
+            <View
+                style={{
+                    backgroundColor: "#00CED1",
+                    flexDirection: "row",
+                    alignItems: "center",
           padding: 10,
         }}
       >
-        <Pressable
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginHorizontal: 7,
-            gap: 10,
-            backgroundColor: "white",
+                <Pressable
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                        marginHorizontal: 7,
+                        gap: 10,
+                        backgroundColor: "white",
             borderRadius: 5,
-            height: 38,
-            flex: 1,
+                        height: 38,
+                        flex: 1,
           }}
         >
-          <AntDesign
-            style={{ paddingLeft: 10 }}
-            name="search1"
+                    <AntDesign
+                        style={{ paddingLeft: 10 }}
+                        name="search1"
             size={24}
             color="black"
           />
           <TextInput placeholder="Search for products" />
-        </Pressable>
-        <Feather name="mic" size={24} color="black" />
-      </View>
+                </Pressable>
+                <Feather name="mic" size={24} color="black" />
+            </View>
 
-      <View style={{ padding: 10, flexDirection: "row", alignItems: "center" }}>
-        <Text style={{ fontSize: 18, fontWeight: "400" }}>Subtotal : </Text>
-        <Text style={{ fontSize: 20, fontWeight: "bold" }}>{total}</Text>
-      </View>
+            <View style={{ padding: 10, flexDirection: "row", alignItems: "center" }}>
+                <Text style={{ fontSize: 18, fontWeight: "400" }}>Subtotal : </Text>
+                <Text style={{ fontSize: 20, fontWeight: "bold" }}>{total}</Text>
+            </View>
       <Text style={{ marginHorizontal: 18 }}>EMI details Available</Text>
 
-      <Pressable
-        style={{
-          backgroundColor: "#FFC72C",
-          padding: 10,
-          borderRadius: 5,
-          justifyContent: "center",
-          alignItems: "center",
-          marginHorizontal: 10,
-          marginTop: 10,
+            <Pressable
+                style={{
+                    backgroundColor: "#FFC72C",
+                    padding: 10,
+                    borderRadius: 5,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginHorizontal: 10,
+                    marginTop: 10,
         }}
       >
         <Text>Proceed to Buy ({cart.length}) items </Text>
-      </Pressable>
+            </Pressable>
 
-      <Text
-        style={{
-          height: 1,
-          borderColor: "#D0D0D0",
-          borderWidth: 1,
-          marginTop: 16,
+            <Text
+                style={{
+                    height: 1,
+                    borderColor: "#D0D0D0",
+                    borderWidth: 1,
+                    marginTop: 16,
         }}
       />
 
-      <View style={{ marginHorizontal: 10 }}>
-        {cart?.map((item, index) => {
-          <View>
-            <Pressable>
-              <View>
-                <Image
-                  style={{ width: 140, height: 140, resizeMode: "contain" }}
-                  source={{ uri: item?.image }}
-                />
-              </View>
-            </Pressable>
-          </View>;
-        })}
-      </View>
-    </ScrollView>
-  );
+            <View style={{ marginHorizontal: 10 }}>
+                {cart?.map((item, index) => (
+                    <View
+                        style={{
+                            backgroundColor: "white",
+                            marginVertical: 10,
+                            borderBottomColor: "#F0F0F0",
+                            borderWidth: 2,
+                            borderLeftWidth: 0,
+                            borderTopWidth: 0,
+                            borderRightWidth: 0,
+                        }}
+                        key={index}>
+
+                        <Pressable
+                            style={{
+                                marginVertical: 10,
+                                flexDirection: "row",
+                                justifyContent: "space-between",
+                            }}>
+                            <View>
+                                <Image
+                                    style={{ width: 140, height: 140, resizeMode: "contain" }}
+                                    source={{ uri: item?.image }}
+                                />
+                            </View>
+
+                            <View>
+                                <Text numberOfLines={3} style={{width:150,marginTop:10}}>{item?.title}</Text>
+                                <Text  style={{fontSize:20,fontWeight:"bold",marginTop:6}}>{item?.price}</Text>
+
+                                
+                                <Image
+                                    style={{ width: 30, height: 30, resizeMode: "contain" }}
+                                    source={{
+                                        uri: "https://assets.stickpng.com/thumbs/5f4924cc68ecc70004ae7065.png",
+                                    }}/>
+                                <Text style={{ color: "green" }}>In Stock</Text>
+                                {/* <Text style={{ fontWeight: "500", marginTop: 6 }}>
+                                    {item?.rating?.rate} ratings
+                                </Text> */}
+                            </View>
+                        </Pressable>
+
+                        <Pressable style={{marginTop:15, marginBottom:10,flexDirection:"row",alignItems:"center",gap:10}}>
+                         <View style={{flexDirection:"row",alignItems:"center",paddingHorizontal:10,paddingVertical:5,borderRadius:7}}>
+                             <Pressable style ={{backgroundColor:"#D0D0D0",padding:7,borderTopLeftRadius:6,borderBottomLeftRadius:6}}> 
+
+                             <AntDesign name="delete" size={24} color="black" />
+                             </Pressable>
+
+                             <Pressable style={{backgroundColor:"white",paddingHorizontal:18,paddingVertical:6}}>
+                               
+                               <Text>{item?.quantity}</Text>
+
+                             </Pressable>
+
+
+                             <Pressable onPress={()=> increaseQuantity(item)} style ={{backgroundColor:"#D0D0D0",padding:7,borderTopLeftRadius:6,borderBottomLeftRadius:6}}> 
+                                
+                             <AntDesign name="plus" size={24} color="black" /> 
+
+                             </Pressable>
+
+                         </View>
+
+
+                        <Pressable style={{backgroundColor:"white",paddingHorizontal:8,paddingVertical:18,borderRadius:5,borderColor:"#C0C0C0",borderWidth:0.6}}>
+
+<                          Text>Delete</Text>
+
+                      </Pressable>
+
+                        </Pressable>
+
+                        <Pressable style={{flexDirection:"row",alignItems:"center",gap:10,marginBottom:15}}>
+                        <Pressable style={{backgroundColor:"white",paddingHorizontal:8,paddingVertical:18,borderRadius:5,borderColor:"#C0C0C0",borderWidth:0.6}}>
+
+                                <Text>Save for Later</Text>
+                            </Pressable>
+
+                       <Pressable style={{backgroundColor:"white",paddingHorizontal:8,paddingVertical:18,borderRadius:5,borderColor:"#C0C0C0",borderWidth:0.6}}>
+
+                                <Text>See More Like this</Text>
+                            </Pressable>
+                        </Pressable>
+
+                    </View>
+                ))}
+            </View>
+        </ScrollView>
+    );
 };
 
 export default CartScreen;
