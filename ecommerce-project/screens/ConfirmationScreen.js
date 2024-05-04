1954336e7f549945a2f7deea6845eb9c54cb77be
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-05-04 14:06:01 +0530

    bug fixed

--- ecommerce-project/screens/ConfirmationScreen.js
+++ ecommerce-project/screens/ConfirmationScreen.js
@@ -13,6 +13,8 @@ const ConfirmationScreen = () => {
   ];
   const [currentStep, setCurrentStep] = useState(0);
   const [ addresses, setAddresses ] = useState([]);
+  const [selectedAddress, setSelectedAdress] = useState("");
+
   const { userId, setUserId } = useContext(UserType);
 
   useEffect(() => {
@@ -98,42 +100,54 @@ const ConfirmationScreen = () => {
           </Text>
 
           <Pressable>
-            {addresses.map((item, index) => {
-              <Pressable>
-                <Entypo name="circle" size={24} color="black" />
+            {addresses?.map((item, index) => (
+              <Pressable
+                style={{
+                  borderWidth: 1,
+                  borderColor: "#D0D0D0",
+                  padding: 10,
+                  flexDirection: "row",
+                  alignItems: "center",
+                  gap: 5,
+                  paddingBottom: 17,
+                  marginVertical: 7,
+                  borderRadius: 6,
+                }}>
+                {selectedAddress && selectedAddress._id === item?._id ? (
+                  <FontAwesome5 name="dot-circle" size={20} color="#008397" />
+                ) : (
+                  <Entypo
+                    onPress={() => setSelectedAdress(item)}
+                    name="circle"
+                    size={20}
+                    color="gray"/>
+                )}
 
-                <View>
+                <View style={{ marginLeft: 6 }}>
                   <View
                     style={{
                       flexDirection: "row",
                       alignItems: "center",
                       gap: 3,
-                    }}
-                  >
-                    <Text style={{ fontSize: 15, fontWeight: "bold" }}>
-                      {item?.name}
+                    }}>
+                    <Text style={{ fontSize: 15, fontWeight: "bold" }}>{item?.name}
                     </Text>
                     <Entypo name="location-pin" size={24} color="red" />
-                  </View>
+                    </View>
 
-                  <Text style={{ fontSize: 15, color: "#181818" }}>
-                    {item?.houseNo},{item?.landmark}
+                  <Text style={{ fontSize: 15, color: "#181818" }}>{item?.houseNo}, {item?.landmark}
                   </Text>
 
-                  <Text style={{ fontSize: 15, color: "#181818" }}>
-                    {item?.street}
+                  <Text style={{ fontSize: 15, color: "#181818" }}>{item?.street}
                   </Text>
 
-                  <Text style={{ fontSize: 15, color: "#181818" }}>
-                    India,Erode
+                  <Text style={{ fontSize: 15, color: "#181818" }}>India, Delhi
                   </Text>
 
-                  <Text style={{ fontSize: 15, color: "#181818" }}>
-                    phone No :{item?.mobileNo}
+                  <Text style={{ fontSize: 15, color: "#181818" }}>phone No : {item?.mobileNo}
                   </Text>
 
-                  <Text style={{ fontSize: 15, color: "#181818" }}>
-                    pin code:{item?.postalCode}
+                  <Text style={{ fontSize: 15, color: "#181818" }}>pin code : {item?.postalCode}
                   </Text>
 
                   <View
@@ -141,9 +155,8 @@ const ConfirmationScreen = () => {
                       flexDirection: "row",
                       alignItems: "center",
                       gap: 10,
-                      marginTop: 10,
-                    }}
-                  >
+                      marginTop: 7,
+                    }}>
                     <Pressable
                       style={{
                         backgroundColor: "#F5F5F5",
@@ -152,8 +165,7 @@ const ConfirmationScreen = () => {
                         borderRadius: 5,
                         borderWidth: 0.9,
                         borderColor: "#D0D0D0",
-                      }}
-                    >
+                      }}>
                       <Text>Edit</Text>
                     </Pressable>
 
@@ -165,8 +177,7 @@ const ConfirmationScreen = () => {
                         borderRadius: 5,
                         borderWidth: 0.9,
                         borderColor: "#D0D0D0",
-                      }}
-                    >
+                      }}>
                       <Text>Remove</Text>
                     </Pressable>
 
@@ -178,14 +189,32 @@ const ConfirmationScreen = () => {
                         borderRadius: 5,
                         borderWidth: 0.9,
                         borderColor: "#D0D0D0",
-                      }}
-                    >
+                      }}>
                       <Text>Set as Default</Text>
                     </Pressable>
                   </View>
+
+                  <View>
+                    {selectedAddress && selectedAddress._id === item?._id && (
+                      <Pressable
+                        onPress={() => setCurrentStep(1)}
+                        style={{
+                          backgroundColor: "#008397",
+                          padding: 10,
+                          borderRadius: 20,
+                          justifyContent: "center",
+                          alignItems: "center",
+                          marginTop: 10,
+                        }}>
+                        <Text style={{ textAlign: "center", color: "white" }}>
+                          Deliver to this Address
+                        </Text>
+                      </Pressable>
+                    )}
+                  </View>
                 </View>
-              </Pressable>;
-            })}
+              </Pressable>
+            ))}
           </Pressable>
         </View>
       )}
